40ad1696d4a777aceecc6c0c3d6db527900007dd
kamiltlalka <56155489+kamiltlalka@users.noreply.github.com>
2023-01-20 14:20:35 +0100

    cleaning up

--- scripts/Brick_vars.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Brick_vars : MonoBehaviour
-{
-    public int hitpoints;
-    public int type; // 0-default 9-hearth
-}
--- scripts/ListBrick.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[System.Serializable]
-public class ListBrick //structure to store bricks prefabs 
-{
-    public Color color;
-    public GameObject pref;
-}
-
